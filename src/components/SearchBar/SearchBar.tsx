826fc23d15ae28c3577f9bc8905654cd05bdf82f
Timóteo Stifft <timoteostifft@gmail.com>
2022-07-25 23:49:49 -0300

    feat: create search bar component

---
+++ src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './SearchBar.module.css'
+
+import add from '../../assets/add.svg'
+
+const SearchBar: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <input type="text" placeholder='Adicione uma nova tarefa' />
+      <button>
+        Criar
+        <img src={add} alt="add todo" />
+      </button>
+    </div>
+  )
+}
+
+export default SearchBar;
