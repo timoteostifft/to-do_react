fa9cc2df9a4a56ddf71e1e76e9adf096421e4665
Timóteo Stifft <timoteostifft@gmail.com>
2022-07-26 15:24:14 -0300

    feat: create delete task method and add custom task type

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import Header from "./components/Header/Header"
-import InputBar from "./components/InputBar/InputBar"
-import TasksHeader from "./components/TasksHeader/TasksHeader"
+import { ChangeEvent, FormEvent, useState } from "react"
 
 import { v4 as uuidv4 } from 'uuid';
 
+import Header from "./components/Header/Header"
+import InputBar from "./components/InputBar/InputBar"
 import Task from "./components/Task/Task"
+import TasksHeader from "./components/TasksHeader/TasksHeader"
+
+import './global.css'
+import styles from './App.module.css'
 
 import clipboard from './assets/clipboard.svg'
 
-import styles from './App.module.css'
-import './global.css'
-import { ChangeEvent, FormEvent, useState } from "react"
+import TTask from "./types/TTask"
 
 export function App() {
 
-  const [tasks, setTasks] = useState<string[]>([])
+  const [tasks, setTasks] = useState<TTask[]>([])
 
-  const [newTask, setNewTask] = useState('')
+  const [newTaskText, setNewTaskText] = useState<string>('')
 
   function handleNewTaskChange(event: ChangeEvent<HTMLInputElement>) {
     event.target.setCustomValidity("")
-    setNewTask(event.target.value)
+    setNewTaskText(event.target.value)
   }
 
   function handleCreateNewTask(event: FormEvent) {
     event.preventDefault()
 
+    const newTask: TTask = { content: newTaskText, uuid: uuidv4() }
+
     setTasks([...tasks, newTask])
-    setNewTask('')
+    setNewTaskText('')
+  }
+
+  function handleDeleteTask(taskToDeleteUuid: string) {
+    const tasksWithoutDeletedOne = tasks.filter(task => {
+      return task.uuid !== taskToDeleteUuid
+    })
+
+    setTasks(tasksWithoutDeletedOne)
   }
 
   return (
     <div>
       <Header />
       <div className={styles.wrapper}>
-        <InputBar handleNewTaskChange={handleNewTaskChange} handleCreateNewTask={handleCreateNewTask} content={newTask} />
+        <InputBar handleNewTaskChange={handleNewTaskChange} handleCreateNewTask={handleCreateNewTask} content={newTaskText} />
 
         <TasksHeader tasks={tasks} />
 
@@ -46,7 +58,7 @@ export function App() {
           </div>
           : (tasks.map(
             task =>
-              <Task key={uuidv4()} content={task} />
+              <Task key={task.uuid} uuid={task.uuid} content={task.content} onDeleteTask={handleDeleteTask} />
           ))
         }
       </div>
