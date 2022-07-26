f57949f8a462093575b3309bfb1d277f3952d283
Timóteo Stifft <timoteostifft@gmail.com>
2022-07-26 14:44:20 -0300

    feat: create create new task methods

--- src/components/InputBar/InputBar.tsx
+++ src/components/InputBar/InputBar.tsx
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { ChangeEvent, ChangeEventHandler, FormEvent, MouseEventHandler, useState } from 'react';
 
 import styles from './InputBar.module.css'
 
 import add from '../../assets/add.svg'
 
-const SearchBar: React.FC = () => {
+interface InputBarProps {
+  handleNewTaskChange: ChangeEventHandler<HTMLInputElement>;
+  handleCreateNewTask: MouseEventHandler<HTMLButtonElement>;
+  content: string;
+}
+
+const SearchBar: React.FC<InputBarProps> = ({ handleNewTaskChange, handleCreateNewTask, content }) => {
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder='Adicione uma nova tarefa' />
-      <button>
+      <input type="text" placeholder={'Adicione uma nova tarefa'} onChange={handleNewTaskChange} value={content} />
+      <button onClick={handleCreateNewTask}>
         Criar
         <img src={add} alt="add todo" />
       </button>
